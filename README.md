71a85398fb804ce7fa37e814a16615fa26cc5247
Anastasia Semisorova <semisorova29@mail.ru>
2024-10-13 11:24:45 +0300

    project_6 junit5 files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # qa_java
 QA Java Project
+
+
+#### Для формирования отчета jacoco:
+mvn clean test (запускаем тесты)
+
+/c/Program\ Files/apache-maven-3.9.8/bin/mvn.cmd jacoco:report (для указания пути мавен и создания отчета)
+
+cd target/site/jacoco (переходим в ветку таргет)
+
+explorer.exe index.html (открываем отчет в браузере)
+
+#### Для отчета если мавен правильно натроен в переменной окружения:
+mvn clean test
+
+mvn jacoco:report
--- src/test/java/LionParameterizedTest.java
+++ src/test/java/LionParameterizedTest.java
@@ -1,5 +1,31 @@
 import com.example.Feline;
 import com.example.Lion;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LionParameterizedTest {
+
+    // Параметризованный тест с JUnit 5
+    @ParameterizedTest
+    @CsvSource({
+            "Самец, true",  // Для самца ожидается наличие гривы
+            "Самка, false"  // Для самки ожидается отсутствие гривы
+    })
+    public void testDoesHaveMane(String sex, boolean expectedHasMane) throws Exception {
+        // Создаем экземпляр класса Lion с заданным полом
+        Lion lion = new Lion(sex, new Feline());
+
+        // Проверяем соответствует ли результат ожиданиям
+        assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
+}
+
+
+
+/*import com.example.Feline;
+import com.example.Lion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -36,6 +62,6 @@ public class LionParameterizedTest {
         // Проверяем соответствует ли результат ожиданиям
         assertEquals(expectedHasMane, lion.doesHaveMane());
     }
-}
+}*/
 
 
